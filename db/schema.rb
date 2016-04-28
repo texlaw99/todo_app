2d99839a44b01d8a6fd4c79a33812c1b58f18df7
texlaw99 <nicfranklin@gmail.com>
2016-04-27 22:37:27 -0500

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160427155050) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.string   "title"
     t.text     "notes"
